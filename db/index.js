ad393cb18146392dd030b5035b327c08d9a3e856
Madeline A <madeline.ashley2@gmail.com>
2022-10-27 12:57:35 -0500

    first push to heroku

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/<makersDatabase>'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/makersDatabase')
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
--- server.js
+++ server.js
@@ -22,44 +22,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api',routes);
 app.use("*",(req,res)=>res.status(404).json({error:"Page Not Found"}))
+app.use(express.static(`${__dirname}/client/build`))
 
-// app.get('/', (req,res) =>{
-//   res.send("This is the Landing Page")
-// })
-
-// app.get(`/about`,(req,res)=>{
-//   res.send("This is the About page")
-// })
-
-// // app.get(`/makers`, makerController.getMakers)
-// app.get("/makers",async (req,res)=>{
-//   const makers=await Maker.find({})
-//   res.json(makers)
-// })
-
-// app.get("/makers/:id",async (req,res)=>{
-//   const{id}=req.params
-//   const maker=await Maker.findById(id)
-//   res.json(maker)
-// })
-
-// app.get(`/makers/add`,(req,res)=>{
-//   res.send("This is where the add maker form will go")
-// })
-
-// app.post(`/makers/add`,(req,res)=>{
-//   res.send("This is the post route for input data from the form")
-// })
-
-// app.get(`/login`,loginController.userLogin)
-
-
-// app.get('/search', (req, res) => {
-//     res.send("This is the search page")
-//   })
-
-
-//   app.get(`/makers/:makerId`, makerController.getMakersById)
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+ })
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
