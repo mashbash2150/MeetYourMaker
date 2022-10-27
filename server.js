a3d3128409db22a3ba293e01d42096c53cce08fd
Madeline A <madeline.ashley2@gmail.com>
2022-10-27 16:06:45 -0500

    updated .env

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api',routes);
-app.use("*",(req,res)=>res.status(404).json({error:"Page Not Hello"}))
+app.use("*",(req,res)=>res.status(404).json({error:"Page Not Found"}))
 app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/*', (req, res) => {
