12ad6dc4f9f7598ba063e1014078f442914c9742
Madeline A <madeline.ashley2@gmail.com>
2022-10-27 15:22:09 -0500

    Heroku troubleshooting

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/api',routes);
-app.use("*",(req,res)=>res.status(404).json({error:"Page Not Found"}))
+app.use('/',routes);
+app.use("*",(req,res)=>res.status(404).json({error:"Page Not Hello"}))
 app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/*', (req, res) => {
